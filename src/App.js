7c942656921226ef96604afb84243d4ab092ba80
Helio Morais <heliogov20@gmail.com>
2023-04-05 19:47:26 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -11,13 +11,40 @@ class App extends React.Component {
     attr2: 0,
     attr3: 0,
     rare: 'normal',
-    trunfo: true,
+    trunfo: false,
+    imgUrl: '',
+    buttonDisabled: true,
+
   };
 
   handleChange = ({ target }) => {
     this.setState(() => ({
       [target.name]: target.type === 'checkbox' ? target.checked : target.value,
-    }));
+    }), () => {
+      const {
+        name, description, attr1, attr2, attr3, image, rare,
+      } = this.state;
+
+      if (target.name === 'image') {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${image.toUpperCase()}`)
+          .then((response) => response.json())
+          .then((data) => this.setState({
+            imgUrl: data.sprites.other['official-artwork'].front_default,
+          }))
+          .catch(() => this.setState((previousState) => previousState));
+      }
+      const valueTotal = 210;
+      const maxAtt = 90;
+      const oneTest = name && description && image && rare;
+      const twoTest = parseInt(attr1, 10) + parseInt(attr2, 10)
+          + parseInt(attr3, 10) <= valueTotal;
+      const threeTest = attr1 <= maxAtt && attr2 <= maxAtt && attr3 <= maxAtt;
+      const fourTest = attr1 >= 0 && attr2 >= 0 && attr3 >= 0;
+      this.setState(() => ({
+        buttonDisabled: !(oneTest && twoTest
+          && threeTest && fourTest),
+      }));
+    });
   };
 
   render() {
