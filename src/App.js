e9e5523fbd83baca2a6101a595eda04c8683547f
Helio Morais <heliogov20@gmail.com>
2023-04-05 17:16:05 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -3,19 +3,51 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    image: '',
+    attr1: 0,
+    attr2: 0,
+    attr3: 0,
+    rare: 'normal',
+    trunfo: true,
+  };
+
+  handleChange = ({ target }) => {
+    this.setState(() => ({
+      [target.name]: target.type === 'checkbox' ? target.checked : target.value,
+    }));
+  };
+
   render() {
+    const {
+      name, description, attr1, attr2, attr3, image, rare, trunfo,
+      buttonDisabled, hasTrunfo, imgUrl } = this.state;
     return (
       <main>
-        <Form />
+        <Form
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+          hasTrunfo={ hasTrunfo }
+          onInputChange={ this.handleChange }
+          isSaveButtonDisabled={ buttonDisabled }
+        />
         <Card
-          cardName=""
-          cardDescription=""
-          cardAttr1=""
-          cardAttr2=""
-          cardAttr3=""
-          cardImage=""
-          cardRare=""
-          cardTrunfo=""
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ imgUrl }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
         />
       </main>
 
